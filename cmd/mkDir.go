d3d08cab77ce5188fb7ef5f31e3636c54e3c39e5
BKR-dev <giga@sigmachad.quest>
2023-09-14 13:52:16 +0200

    working on flags for delete dir cmd

--- cmd/mkDir.go
+++ cmd/mkDir.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.AddCommand(mkDir)
+	mkDir.PersistentFlags().String("rm-all", "", "remove all directories")
 }
 
 var (
@@ -18,6 +22,15 @@ var (
 )
 
 func mkDirCmd(cmd *cobra.Command, args []string) {
+	fmt.Println(args)
+	rmAll, err := cmd.Flags().GetString("rm-all")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(rmAll)
+	rm := cmd.Flag("rm-all")
+	fmt.Println(rm)
+	os.Exit(1)
 	// util.MakeDirSkeleton()
 	// util.CleanUpAllDirs()
 }
